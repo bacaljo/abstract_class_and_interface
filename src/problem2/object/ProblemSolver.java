8cdeddc8680baac5f09de41e97fd17012e098cb1
John Emmanuel Bacalla <john.emmanuel.bacalla@oocl.com>
2020-10-06 19:24:14 +0800

    Create an interface that will define the action to choose affordable mode of transportation designed to be implemented by a person object

---
+++ src/problem2/object/ProblemSolver.java
@@ -0,0 +1,7 @@
+package problem2.object;
+
+import java.util.List;
+
+public interface ProblemSolver {
+    List<ModeOfTransportation> chooseAffordableModeOfTransportation(List<ModeOfTransportation> availableModeOfTransportation);
+}
